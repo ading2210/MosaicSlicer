1569d182c5d054e55c66787f0faeb5090b000c59
Rohan Gupta <simplyrohan10@gmail.com>
2025-03-05 23:50:18 -0800

    Remake sidebar with Cura-like styles

--- src/gui/sidebar.mjs
+++ src/gui/sidebar.mjs
@@ -10,61 +10,19 @@ const tab_template = document.getElementById("tab-template");
 var selected_tab = null;
 var definition = {};
 
-function select_tab(tab) {
-  if (selected_tab)
-    document.querySelector("#sidebar>.tabs-container>.tab.selected").classList.remove("selected");
-  selected_tab = tab;
-  document.querySelector(`#sidebar>.tabs-container>.tab[data-setting="${tab}"]`).classList.add("selected");
-}
-
-function populate_settings() {
-  tab_settings.innerHTML = "";
-  let category = definition.printer.settings[selected_tab];
-
-  for (let setting_id in category.children) {
-    let setting = category.children[setting_id];
-    let template = setting_template.content.cloneNode(true);
-    template.get_slot("label").innerText = setting.label;
-    template.get_slot("unit").innerText = setting.unit || "";
-
-    let input = template.get_slot("value");
-    if (setting.type == "float") {
-      input.type = "number";
-      input.step = "1";
-    }
-    else if (setting.type == "int") {
-      input.type = "number";
-      input.step = "1";
-    }
-    else if (setting.type == "bool") {
-      input.type = "checkbox";
-    }
-
-    tab_settings.append(template);
-  }
-}
-
 export function load_sidebar() {
   definition = active_containers.definitions;
   let settings = definition.printer.settings;
 
   // Populate sidebar tabs
   for (let setting in settings) {
-    let template = tab_template.content.cloneNode(true);
-    let tab = template.get_slot("tab-span");
-    let tab_text = template.get_slot("tab-text");
-    tab.dataset.setting = setting;
-    tab_text.innerText = definition.printer.settings[setting].label;
-    tab.onclick = () => {
-      console.log("Clicked", setting);
-      select_tab(setting);
-      populate_settings();
-    };
-    sidebar_tabs.append(template);
   }
-
-  select_tab(Object.keys(definition.printer.settings)[8]);
-
-  // Populate sidebar settings
-  populate_settings();
 }
+
+// This should be when the settings are populated
+// I'm sorry for using forEach, but its termporary
+document.querySelectorAll(".section-title").forEach((el) => {
+  el.addEventListener("click", () => {
+    el.parentElement.classList.toggle("closed");
+  });
+});
