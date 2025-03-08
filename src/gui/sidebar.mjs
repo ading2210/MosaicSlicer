d902e478bd627dc4a43b6730fa9da29e0f846ac7
Rohan Gupta <simplyrohan10@gmail.com>
2025-03-07 23:24:32 -0800

    search algorithm (not rendered)

--- src/gui/sidebar.mjs
+++ src/gui/sidebar.mjs
@@ -1,8 +1,11 @@
+import { temp } from "three/tsl";
 import { active_containers } from "../settings/index.mjs";
 
 const sections = document.getElementById("sections");
 const section_template = document.getElementById("section-template");
 const setting_template = document.getElementById("setting-template");
+
+var search_index = {}
 
 export function load_sidebar() {
   let definition = active_containers.definitions;
@@ -34,6 +37,7 @@ export function load_sidebar() {
 }
 
 function generate_setting(setting_id, setting) {
+
   let template = setting_template.content.cloneNode(true);
   let value = template.get_slot("value");
   let unit = template.get_slot("unit");
@@ -75,6 +79,9 @@ function generate_setting(setting_id, setting) {
     }
   }
 
+
+  search_index[setting.label] = template.firstElementChild
+
   return template;
 }
 
@@ -111,3 +118,32 @@ function populate_values() {
   let end = performance.now();
   console.log("loaded all settings values in ", Math.round((end - start) * 100) / 100, "ms");
 }
+
+// Search
+function filter_settings(terms) {
+  terms = terms.toLowerCase().split(" ")
+
+  let result_names = [];
+  for (let setting_name in search_index) {
+    let valid = true;
+    for (let term of terms) {
+      if (!setting_name.toLowerCase().split(" ").includes(term)) {
+        valid = false;
+        break;
+      }
+    }
+    if (valid) {
+      result_names.push(setting_name)
+    }
+  }
+  console.log(result_names)
+  // todo: render them
+  //  - find the sections of each setting
+  //  - hide everything except relevent sections
+  //  - open the relevent sections and hide everything except the setting names
+  //  - make sure this is all undone when the search is cleared/editted
+}
+const search_input = document.getElementById('setting-search')
+search_input.addEventListener('input', () => {
+  filter_settings(search_input.value)
+})
