4cf8d98e1eb21033c21d04ad0d2623c2980572b1
Rohan Gupta <simplyrohan10@gmail.com>
2025-03-12 19:13:53 -0700

    Working movement arrows

--- src/gui/viewer/viewer.mjs
+++ src/gui/viewer/viewer.mjs
@@ -13,6 +13,7 @@ const scale_button = document.getElementById("scale-button");
 const x_axis = new THREE.Vector3(1, 0, 0);
 const y_axis = new THREE.Vector3(0, 1, 0);
 const z_axis = new THREE.Vector3(0, 0, 1);
+const axes = [x_axis, y_axis, z_axis];
 
 /** @type {THREE.ArrowHelper[]} */
 var helpers = [];
@@ -78,22 +79,47 @@ function remove_transform_helpers() {
   }
 }
 
+const CONE_HEIGHT = 0.1;
+const CONE_WIDTH = 0.1;
 function toggle_movement() {
   if (helpers.length != 0)
     remove_transform_helpers();
   else {
-    let x_arrow = new THREE.ArrowHelper(x_axis, models[focused].mesh.position, 0.75, 0xff0000, 0.1, 0.1);
-    let y_arrow = new THREE.ArrowHelper(y_axis, models[focused].mesh.position, 0.75, 0x00ff00, 0.1, 0.1);
-    let z_arrow = new THREE.ArrowHelper(z_axis, models[focused].mesh.position, 0.75, 0x0000ff, 0.1, 0.1);
-    for (let arrow of [x_arrow, y_arrow, z_arrow]) {
+    let x_arrow = new THREE.ArrowHelper(x_axis, models[focused].mesh.position, 0.75, 0xff0000, CONE_HEIGHT, CONE_WIDTH);
+    let y_arrow = new THREE.ArrowHelper(y_axis, models[focused].mesh.position, 0.75, 0x00ff00, CONE_HEIGHT, CONE_WIDTH);
+    let z_arrow = new THREE.ArrowHelper(z_axis, models[focused].mesh.position, 0.75, 0x0000ff, CONE_HEIGHT, CONE_WIDTH);
+
+    let initial_pos = models[focused].mesh.position.clone();
+    for (let [i, arrow] of [x_arrow, y_arrow, z_arrow].entries()) {
       helpers.push(arrow);
       renderer.scene.add(arrow);
 
+      /** @type {controls.SceneObject} */
       let sceneobj = Object.create(controls.SceneObject);
       sceneobj.mesh = arrow.cone;
+
+      let cumulative = 0;
       sceneobj.ondrag = (e) => {
-        console.log("dragging arrow");
+        // This needs to be improved...
+        // We should find direction of vector in screen space and track the mouse along that line
+        // Mapping that line between screen space, line space, and then movement space will be a bit trickey but this could help
+        // https://discourse.threejs.org/t/how-to-converting-world-coordinates-to-2d-mouse-coordinates-in-threejs/2251
+
+        cumulative += i == 0 ? e.movementX : (i == 1 ? -e.movementY : -e.movementX);
+
+        // I don't like making so many copies but whatever
+        // Also Three.js doens't support vector operations with normal math syntax (like + or =) which is why I'm using `.add` and `.copy` functions respectively
+        models[focused].mesh.position.copy(initial_pos.clone().add(axes[i].clone().setLength(cumulative * 0.005)));
+
+        x_arrow.position.copy(models[focused].mesh.position);
+        y_arrow.position.copy(models[focused].mesh.position);
+        z_arrow.position.copy(models[focused].mesh.position);
       };
+      sceneobj.onclick = () => { // onclick is also called when drag is released
+        cumulative = 0;
+        initial_pos = models[focused].mesh.position.clone();
+      };
+
       controls.scene_objects[arrow.cone.uuid] = sceneobj;
     }
   }
