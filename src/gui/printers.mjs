c902deb68a84f967e76e936a2af253cce38bf9d0
ading2210 <ading2019@gmail.com>
2025-03-20 06:24:22 -0400

    add logo to extension and readme

--- src/gui/printers.mjs
+++ src/gui/printers.mjs
@@ -2,4 +2,3 @@ export const printer_prefs_container = document.getElementById("prefs-printer");
 export const printers_list_div = document.getElementById("prefs-printers-list");
 export const printer_options_container = document.getElementById("prefs-printer-options");
 export const printer_options_template = document.getElementById("printer-options-template");
-
